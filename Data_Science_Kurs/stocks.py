2a8d8aef994a24b88df252e523aeac28c4ee1e9f
Grischa85 <95318238+Grischa85@users.noreply.github.com>
2022-04-05 20:37:40 +0200

    Update stocks.py

--- Data_Science_Kurs/stocks.py
+++ Data_Science_Kurs/stocks.py
@@ -11,10 +11,6 @@ Company = "BMW.DE"
 
 stock = yf.download(Company, start='2021-1-1', end=datetime.today())
 close = stock['Adj Close']
-date = stock.index[:]
-rsi = close, date
-RSI = talib.RSI(close)
-k, d = talib.STOCH(RSI, RSI, RSI)
 ema50 = talib.EMA(close, timeperiod=50)
 ema200 = talib.EMA(close, timeperiod=200)
 
@@ -24,12 +20,4 @@ ax1.set_title(f"{Company}")
 ax1.plot(stock['Close'])
 ax1.plot(ema50)
 ax1.plot(ema200)
-# STOCH
-ax2.plot(k, color='lightblue')
-ax2.axhline(20, linestyle='--', color="r")
-ax2.axhline(80, linestyle="--", color="g")
-# RSI
-ax2.plot(RSI)
-ax2.axhline(30, linestyle='--', color="r")
-ax2.axhline(70, linestyle="--", color="g")
 plt.show()
